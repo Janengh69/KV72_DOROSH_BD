43d44e9476f48124d1f813d5c11f3c005beb5f35
Karyna Dorosh <karyna.dorosh@gmail.com>
2019-10-29 00:07:09 +0200

    full text search added

--- BD_lab2/controller.py
+++ BD_lab2/controller.py
@@ -3,68 +3,38 @@ from model import *
 from cargo_d import *
 import sys
 import sys  # sys нужен для передачи argv в QApplication
-from PyQt5 import QtWidgets
+
+# TODO SELECT - MVC
+# TODO ERROR DISPLAY
+# TODO FULL TEXT TSVECTOR
 
 class CargoController( Ui_Dialog):
     def __init__(self, MainWindow,db):
         super().__init__(MainWindow)
         self.db = db
         self.columns = ' '
-        self.pushButton.clicked.connect(self.getValues)
-
-    def getValues(self):
-        from_range = int(self.textEdit.toPlainText())
-        to_range = int (self.textEdit_2.toPlainText())
-        checked = str(not self.checkBox.checkState()==0).lower()
-
-        req = " SELECT * FROM (SELECT * FROM cargo INNER JOIN worker ON cargo.worker_id = worker.id) AS result WHERE (estimated_value BETWEEN {0} AND {1}) AND \"Delivered\" = {2};".format(from_range, to_range,checked)
-        #req = "select to_tsvector('note');"
-        result = self.db.get_request(req)
-        r_str = ""
-        for i in result:
-            for st in i:
-                r_str += str(st) + "    "
-            r_str += "\n"
-        for word in self.db.colnames:
-            self.columns += word + "\t"
-        self.columns += '\n'
-        self.plainTextEdit.setPlainText(self.columns + r_str)
+        self.pushButton.clicked.connect(self.get_values_call)
+    
+    def get_values_call(self):
+        self.db.getValues(self)
 
 class Controller(Ui_Database):
-    def requestFormat(self, comboTable, comboAction, textAction):
-        if comboAction == 'delete':
-            self.ui.Flag = self.db.delete_request(comboTable, textAction)
-            if not self.ui.Flag:
-                self.error.setText('Error in sequence')
-            else:
-                self.error.setText('Done')
-        elif comboAction == 'insert':
-            self.ui.Flag = self.db.insert_request(comboTable, textAction)
-            if not self.ui.Flag:
-                self.error.setText('Error in sequence')
-            else:
-                self.error.setText('Done')
-        elif comboAction == 'update':
-            self.ui. Flag = self.db.update_request(comboTable, textAction)
-            print(self.ui.Flag)
-            if not self.ui.Flag:
-                self.error.setText('Error in sequence')
-            else:
-                self.error.setText('Done')
-
-    def gen_values(self):
-        self.db.generate_values()
+    
+    def gen_values_call(self):
+        self.db.gen_values(self)
+            
 
     def __init__(self, MainWindow):
         self.ui = Ui_Database(MainWindow)
         self.db = Database()
         super().__init__(MainWindow)
+        self.gen_label.setText('')
 
         self.pushButton.clicked.connect(self.saveInfo)
-        self.genData.clicked.connect(self.gen_values)
+        self.genData.clicked.connect(self.gen_values_call)
         self.pushButton_2.clicked.connect(self.showDialog)
-        self.search.clicked.connect(self.full_text_search)
-        self.pushButton_3.clicked.connect(self.full_str)
+        self.search.clicked.connect(self.full_text_search_call)
+        self.pushButton_3.clicked.connect(self.full_str_call)
         self.comboTable = None
         self.comboAction = None
         self.textAction = None
@@ -79,49 +49,19 @@ class Controller(Ui_Database):
         self.comboAction = self.action.currentText()
         self.comboTable = self.table.currentText()
         self.textAction = self.textEdit.toPlainText()
-        print(self.comboAction, self.comboTable, self.textAction)
-        self.requestFormat(self.comboTable, self.comboAction, self.textAction)
+        self.db.requestFormat(self.comboTable, self.comboAction, self.textAction, self)
         if not self.Flag:
             self.error_dialog = QtWidgets.QErrorMessage()
-            self.error_dialog.showMessage('Unable to perceive the rquest')
+            self.error_dialog.showMessage('Unable to perceive the request')
     def showDialog(self):
         self.window.show()
 
-    def full_text_search(self):
-        self.full_text = self.textSearch.toPlainText()
-        self.full_search_table = self.full_text_box.currentText()
-        self.full_text = self.full_text.split('=')
-        try:
-            req = "SELECT * FROM {1} WHERE {0} LIKE '%{2}%'".format(self.full_text[0], self.full_search_table, " ".join(self.full_text[1].split()))
-            name = str(self.db.get_request(req))
-            r_str = ''
-            for i in name:
-                for st in i:
-                    r_str += str(st) + "    "
-                r_str += "\n"
-            for word in self.db.colnames:
-                self.columns += word + "    "
-            self.columns += '\n'
-            self.textSearch.setText(self.columns + name)
-        except:
-            self.textSearch.setText("Error")
+    
+    def full_text_search_call(self):
+        self.db.word_only(self)
 
-
-    def full_str(self):
-        self.full_text = self.textSearch.toPlainText()
-        self.full_search_table = self.full_text_box.currentText()
-        self.full_text = self.full_text.split('=')
-        req = "SELECT * FROM {1} WHERE {0} LIKE '{2}'".format(self.full_text[0], self.full_search_table, " ".join(self.full_text[1].split()))
-        name = str(self.db.get_request(req))
-        r_str = ''
-        for i in name:
-            for st in i:
-                r_str += str(st) + "    "
-            r_str += "\n"
-        for word in self.db.colnames:
-            self.columns += word + "    "
-        self.columns += '\n'
-        self.textSearch.setText(self.columns + name)
+    def full_str_call(self):
+        self.db.full_string(self)
 
 if __name__ == '__main__':
     app = QtWidgets.QApplication(sys.argv)
--- BD_lab2/model.py
+++ BD_lab2/model.py
@@ -2,7 +2,9 @@ import psycopg2 as ps
 from configparser import ConfigParser
 import datetime
 from datetime import timedelta
-
+import pandas as pd
+import plotly
+from terminaltables import AsciiTable
 import json
 
 
@@ -22,20 +24,24 @@ class Database:
     def get_request(self, req):
         try:
             cursor = self.conn.cursor()
-            print(req)
             cursor.execute(req)
             self.conn.commit()
             self.colnames = [desc[0] for desc in cursor.description]
-            print(self.colnames)
             return cursor.fetchall()
         except(Exception, ps.DatabaseError, ps.ProgrammingError) as error:
-            print(error)
             self.conn.rollback()
+            self.gen_error = error
+            self.erFlag= True
+            print(error)
             return False
 
 
     def __init__(self):
         self.conn = None
+        self.error = ''
+        self.gen_error = ''
+        self.erFlag = True
+        self.Gen = True
         self.colnames = list()
         try:
             params = self.config('config.ini')
@@ -48,7 +54,6 @@ class Database:
         return self.request("DELETE FROM {0} WHERE {1};".format(action, text))
 
     def insert_request(self, table, text):
-        print(text)
         enter = [list.split('=') for list in text.split(',')] #devided values
         values = arguments = str()
         for word in enter:
@@ -56,7 +61,6 @@ class Database:
             values += word[1] + ','
         arguments = arguments[:-1]
         values = values[:-1]
-        print(values)
         return self.request("INSERT INTO {0} ({1}) VALUES ({2}) ".format(table, arguments, values))
 
 
@@ -65,15 +69,31 @@ class Database:
             cursor = self.conn.cursor()
             print(req)
             cursor.execute(req)
-            print('sdd')
             self.conn.commit()
-            print("fgnkjfg")
             return True
         except(Exception, ps.DatabaseError, ps.ProgrammingError) as error:
             print(error)
+            self.error = error
             self.conn.rollback()
             return False
 
+    def getValues(self, Cargo):
+        from_range = int(Cargo.textEdit.toPlainText())
+        to_range = int (Cargo.textEdit_2.toPlainText())
+        checked = str(not Cargo.checkBox.checkState()==0).lower()
+
+
+        req = " SELECT * FROM (SELECT * FROM cargo INNER JOIN worker ON cargo.worker_id = worker.id) AS result WHERE (estimated_value BETWEEN {0} AND {1}) AND delivered = {2};".format(from_range, to_range,checked)
+        result = self.get_request(req)
+        r_str = ""
+        for i in result:
+            for st in i:
+                r_str += str(st) + "    "
+            r_str += "\n"
+        for word in self.colnames:
+            Cargo.columns += word + "\t"
+        Cargo.columns += '\n'
+        Cargo.plainTextEdit.setPlainText(Cargo.columns + r_str)
 
     def update_request(self, table, text):
         property = text.split('\n')
@@ -87,21 +107,118 @@ class Database:
     def generate_values(self):
         with open('data.json', 'r+') as f:
             data = json.load(f)
-        print(data)
+        self.Gen = False
         start_last_number = data['last_number']+1
         start_estimated_value = data['estimated_value']+1
-        self.request("INSERT INTO department (address, number_d, d_type, street_number) VALUES (get_random_string (9),generate_series({0}, {1}), 'cargo', generate_series({0}, {1}));".format(start_estimated_value, start_estimated_value+10))
-        self.request("INSERT INTO worker (id, full_name, position, working_hours, salary, dep_number) VALUES (generate_series({0},{1}), md5(random()::text), 'manager' , md5(random()::text), generate_series(10000, 10000+10), generate_series({0}, {1}));".format(start_estimated_value, start_estimated_value+10 ))
-        self.request("INSERT INTO client (full_name, client_type, client_number) VALUES (get_random_string (9), 'sender',generate_series({0}, {1}));".format(start_last_number, start_last_number+10))
-        self.request("INSERT INTO cargo (barcode, cargo_type, estimated_value, client_id, worker_id, \"Delivered\" ) VALUES (generate_series({2}, {3}), md5(random()::text), generate_series({0},{1}), generate_series({2},{3}), generate_series({0},{1}), true);".format(start_estimated_value, start_estimated_value+10, start_last_number, start_last_number+10))
-        self.request("INSERT INTO packing (packing_code, packing_type, amount, price, weight, cargo_barcode, is_embedded) VALUES (generate_series({0}, {1}), get_random_string (9), generate_series({2}, {3}), generate_series({3}, {2}, -1), generate_series({2}, {3}), generate_series({0}, {1}), false);".format(start_last_number, start_last_number+10, 10, 20))
-        self.request("INSERT INTO ref_worker_cargo (cargo_barcode, worker_id, time) VALUES (generate_series({0}, {1}), generate_series({2}, {3}), generate_series('{4}'::timestamp, '{5}','24 hours'));".format(start_last_number, start_last_number+10, start_estimated_value, start_estimated_value+10, str(datetime.datetime.now()), str( datetime.datetime.now() + timedelta(days=10))))
-        
-        self.request("INSERT INTO ref_client_worker ( worker_id, client_number, time) VALUES (generate_series({2}, {3}), generate_series({0}, {1}), generate_series('{4}'::timestamp, '{5}','24 hours'));".format(start_last_number, start_last_number+10, start_estimated_value, start_estimated_value+10,  str(datetime.datetime.now()), str( datetime.datetime.now() + timedelta(days=10))))
-        data = {'last_number': start_last_number + 10, 'estimated_value': start_estimated_value + 10}
+        amount = 20
+        self.Gen= self.request("INSERT INTO department (address, number_d, d_type, street_number) VALUES (get_random_string (9),generate_series({0}, {1}), 'cargo', generate_series({0}, {1}));".format(start_estimated_value, start_estimated_value+amount))
+
+        self.Gen= self.request("INSERT INTO worker (id, full_name, position, working_hours, salary, dep_number) VALUES (generate_series({0},{1}), md5(random()::text), 'heavier' , md5(random()::text), generate_series(10000, 10000+{2}), generate_series({0}, {1}));".format(start_estimated_value, int(start_estimated_value+amount/2), int(amount/2)))
+        self.Gen= self.request("INSERT INTO worker (id, full_name, position, working_hours, salary, dep_number) VALUES (generate_series({0},{1}), md5(random()::text), 'casher' , md5(random()::text), generate_series(10000+{2}, 10000+{3}), generate_series({0}, {1}));".format(int(start_estimated_value+amount/2+1), start_estimated_value+amount, int(amount/2+1), amount))
+
+        self.Gen= self.request("INSERT INTO client (full_name, client_type, client_number) VALUES (get_random_string (9), 'sender',generate_series({0}, {1}));".format(start_last_number, int(start_last_number+amount/2)))
+        self.Gen= self.request("INSERT INTO client (full_name, client_type, client_number) VALUES (get_random_string (9), 'recipient',generate_series({0}, {1}));".format(int(start_last_number+ amount/2+1), start_last_number+amount))
+        print(start_estimated_value)
+
+        self.Gen= self.request("INSERT INTO cargo (barcode, cargo_type, estimated_value, client_id, worker_id, delivered ) VALUES (generate_series({2}, {3}), md5(random()::text), generate_series({0},{1}), generate_series({2},{3}), generate_series({0},{1}), true);".format(start_estimated_value, int(start_estimated_value+(amount/2)), start_last_number, int(start_last_number+amount/2)))
+        self.Gen= self.request("INSERT INTO cargo (barcode, cargo_type, estimated_value, client_id, worker_id, delivered ) VALUES (generate_series({2}, {3}), md5(random()::text), generate_series({0},{1}), generate_series({2},{3}), generate_series({0},{1}), false);".format(int(start_estimated_value+ amount/2+1), start_estimated_value+amount, int(start_last_number+amount/2+1), start_last_number+amount))
+
+
+        self.Gen= self.request("INSERT INTO packing (packing_code, packing_type, amount, price, weight, cargo_barcode) VALUES (generate_series({0}, {1}), get_random_string (9), generate_series({2}, {3}), generate_series({3}, {2}, -1), generate_series({2}, {3}), generate_series({0}, {1}));".format(start_last_number, start_last_number+amount, amount, 2*amount))
+        self.Gen= self.request("INSERT INTO ref_worker_cargo (cargo_barcode, worker_id, time) VALUES (generate_series({0}, {1}), generate_series({2}, {3}), generate_series('{4}'::timestamp, '{5}','24 hours'));".format(start_last_number, start_last_number+amount, start_estimated_value, start_estimated_value+amount, str(datetime.datetime.now()), str( datetime.datetime.now() + timedelta(days=amount))))
+
+        self.request("INSERT INTO ref_client_worker ( worker_id, client_number, time) VALUES (generate_series({2}, {3}), generate_series({0}, {1}), generate_series('{4}'::timestamp, '{5}','24 hours'));".format(start_last_number, start_last_number+amount, start_estimated_value, start_estimated_value+amount,  str(datetime.datetime.now()), str( datetime.datetime.now() + timedelta(days=amount))))
+        data = {'last_number': start_last_number + amount, 'estimated_value': start_estimated_value + amount}
         with open('data.json', 'w+') as f:
             json.dump(data, f)
 
 
+    def gen_values(self, Controller):
+        print(Controller)
+        self.generate_values()
+        if self.Gen:
+            Controller.gen_label.setText('Done!')
+        else:
+            Controller.gen_label.setText('Error while generating!')
 
+    def requestFormat(self, comboTable, comboAction, textAction, Controller):
+        Controller.gen_label.setText('')
 
+        if comboAction == 'delete':
+            Controller.Flag = self.delete_request(comboTable, textAction)
+            if not Controller.Flag:
+                Controller.error.setText(str(self.error))
+            else:
+                Controller.error.setText('Done')
+        elif comboAction == 'insert':
+            self.Flag = self.insert_request(comboTable, textAction)
+            if not self.Flag:
+                Controller.error.setText(str(self.error))
+            else:
+                Controller.error.setText('Done')
+        elif comboAction == 'update':
+            self.Flag = self.update_request(comboTable, textAction)
+            if not self.Flag:
+                Controller.error.setText(str(self.error))
+            else:
+                Controller.error.setText('Done')
+
+    def full_string(self, Controller):
+        self.columns = str()
+        Controller.full_text = Controller.textSearch.toPlainText().split('=')
+        Controller.full_search_table = Controller.full_text_box.currentText()       # from which table to search
+        print(Controller.full_text)
+        print(Controller.full_search_table)
+        if len(Controller.full_text) == 1:
+            Controller.textSearch.setText('Wrong entering')
+            return
+        req = ''
+        temp = ''
+        req = "SELECT * FROM {1} WHERE  to_tsvector({0}) @@ phraseto_tsquery('{2}');".format(Controller.full_text[0], Controller.full_search_table, " ".join(Controller.full_text[1].split()))
+        name = self.get_request(req)
+        if len(Controller.full_text) == 1:
+            Controller.textSearch.setText('Wrong entering')
+            return
+        if self.erFlag:
+            Controller.textSearch.setText(str(self.gen_error))
+            return
+        for word in name:
+            for i in word:
+                temp += str(i) + ' '
+            temp+= '\n'
+        for word in self.colnames:
+            Controller.columns += word + "          "
+        Controller.columns += '\n'
+        Controller.textSearch.setText(self.columns + temp)
+
+    def word_only(self, Controller):
+        Controller.full_text = Controller.textSearch.toPlainText()
+        Controller.full_search_table = Controller.full_text_box.currentText()
+        Controller.full_text = Controller.full_text.split('=')
+        template = ''
+        if len(Controller.full_text) == 1:
+            Controller.textSearch.setText('Wrong entering')
+            return
+        if self.erFlag:
+            Controller.textSearch.setText(str(self.gen_error))
+            return
+        for word in Controller.full_text[1].strip().split(' '):
+            template += "'" + word + "'" + '::tsquery || '
+        temp=' '
+        temp = str(map(lambda word: "'" + word + "'" + '::tsquery || ',Controller.full_text[1].strip().split(' ')))
+        template = template[:-3]
+        req = "SELECT * FROM {1} WHERE to_tsvector({0}) @@ ({2})".format(Controller.full_text[0].strip(), Controller.full_search_table, template)
+
+        name = self.get_request(req)
+        Controller.columns = str()
+       # table = AsciiTable(name)
+       # print(table.table)
+        temp = str()
+        for word in name:
+            for i in word:
+                temp += str(i) + '  '
+            temp+= '\n'
+        for word in self.colnames:
+            self.colnames += word + "          "
+        self.colnames += '\n'
+        Controller.textSearch.setText(self.colnames + temp)
